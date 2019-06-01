e614718c9dfb8a78ddb711d0017ff17d51841c70
Ifenna Awagu <jawagu@gmail.com>
2019-06-01 10:35:06 +0100

    Add tests to the ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "plug/token"
+import (
+	"bytes"
+	"plug/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -28,28 +32,79 @@ func (program *Program) TokenLiteral() string {
 		return ""
 	}
 }
+func (program *Program) String() string {
+	var out bytes.Buffer
+
+	for _, statement := range program.Statements {
+		out.WriteString(statement.String())
+	}
+
+	return out.String()
+}
 
 type LetStatement struct {
-	Token token.Token		// the token.LET token
-	Name *Identifier
+	Token token.Token // the token.LET token
+	Name  *Identifier
 	Value Expression
 }
 
-func (letStatement *LetStatement) statementNode() {}
+func (letStatement *LetStatement) statementNode()       {}
 func (letStatement *LetStatement) TokenLiteral() string { return letStatement.Token.Literal }
+func (letStatement *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(letStatement.TokenLiteral() + " ")
+	out.WriteString(letStatement.Name.String())
+	out.WriteString(" = ")
+	if letStatement.Value != nil {
+		out.WriteString(letStatement.Value.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token
 	Value string
 }
 
-func (identifier *Identifier) expressionNode()  {}
+func (identifier *Identifier) expressionNode()      {}
 func (identifier *Identifier) TokenLiteral() string { return identifier.Token.Literal }
+func (identifier *Identifier) String() string       { return identifier.Value }
 
 type ReturnStatement struct {
-	Token token.Token
+	Token       token.Token
 	ReturnValue Expression
 }
 
-func (returnStatement *ReturnStatement) statementNode() {}
-func (returnStatement *ReturnStatement) TokenLiteral() string {return returnStatement.Token.Literal}
+func (returnStatement *ReturnStatement) statementNode()       {}
+func (returnStatement *ReturnStatement) TokenLiteral() string { return returnStatement.Token.Literal }
+func (returnStatement *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(returnStatement.TokenLiteral() + " ")
+	out.WriteString(" = ")
+	if returnStatement.ReturnValue != nil {
+		out.WriteString(returnStatement.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (expressionStatement *ExpressionStatement) statementNode() {}
+func (expressionStatement *ExpressionStatement) TokenLiteral() string {
+	return expressionStatement.Token.Literal
+}
+func (expressionStatement *ExpressionStatement) String() string {
+	if expressionStatement.Expression != nil {
+		return expressionStatement.Expression.String()
+	}
+	return ""
+}
---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"plug/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "foo"},
+					Value: "foo",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "bar"},
+					Value: "bar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let foo = bar;" {
+		t.Errorf("program string is wrong, got %q", program.String())
+	}
+}
