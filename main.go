14f2593256db9c8892ba41ce2a23177c454c1b2e
Ifenna Awagu <jawagu@gmail.com>
2019-06-28 18:14:24 +0100

    Allow either file interpretation or repl interpretation from cli

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/user"
 	"plug/repl"
+	"plug/scanner"
 )
 
 func main() {
-	person, err := user.Current()
-	if err != nil {
-		panic(err)
+	if len(os.Args) == 1 {
+		person, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Hello %s! This is the Plug programming language!\n", person.Username)
+		repl.Start(os.Stdin, os.Stdout)
+	} else {
+		filename := os.Args[1]
+		file, err := os.Open(filename)
+		defer file.Close()
+
+		if err != nil {
+			log.Fatal("unable to read file")
+		}
+
+		scanner.Start(file, os.Stdout)
 	}
-	fmt.Printf("Hello %s! This is the Plug programming language!\n", person.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
 }
